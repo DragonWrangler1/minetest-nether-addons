f70b404f3a988998a42cb398392eef6e46bdad3d
DragonWrangler1 <146014546+DragonWrangler1@users.noreply.github.com>
2024-03-22 20:03:19 -0500

    Add Translations

--- tkr_nether/tools.lua
+++ tkr_nether/tools.lua
@@ -1,6 +1,8 @@
+local S = minetest.get_translator(minetest.get_current_modname())
+
 if  minetest.get_modpath("nether_sword_api") then
 minetest.register_tool(":nether:sword_dragon_scale", {
-	description = "Dragon Scale Sword",
+	description = S("Dragon Scale Sword"),
 	inventory_image = "dragon_scale_sword.png",
 	tool_capabilities = {
 		full_punch_interval = 0.3,
@@ -20,7 +22,7 @@ minetest.register_tool(":nether:sword_dragon_scale", {
 else
 
 minetest.register_tool(":nether:sword_dragon_scale", {
-	description = "Dragon Scale Sword",
+	description = S("Dragon Scale Sword"),
 	inventory_image = "dragon_scale_sword.png",
 	tool_capabilities = {
 		full_punch_interval = 0.3,
@@ -38,7 +40,7 @@ minetest.register_tool(":nether:sword_dragon_scale", {
 end
 
 minetest.register_tool(":nether:dragon_scale_pick", {
-	description = ("Dragon Scale Pickaxe"),
+	description = S("Dragon Scale Pickaxe"),
 	inventory_image = "dragon_scale_pickaxe.png",
 	tool_capabilities = {
 		full_punch_interval = 0.8,
@@ -53,7 +55,7 @@ minetest.register_tool(":nether:dragon_scale_pick", {
 })
 
 minetest.register_tool(":nether:dragon_scale_shovel", {
-	description = ("Dragon Scale Shovel"),
+	description = S("Dragon Scale Shovel"),
 	inventory_image = "dragon_scale_shovel.png",
 	tool_capabilities = {
 		full_punch_interval = 0.9,
@@ -68,7 +70,7 @@ minetest.register_tool(":nether:dragon_scale_shovel", {
 })
 
 minetest.register_tool(":nether:dragon_scale_axe", {
-	description = ("Dragon Scale Axe"),
+	description = S("Dragon Scale Axe"),
 	inventory_image = "dragon_scale_axe.png",
 	tool_capabilities = {
 		full_punch_interval = 0.8,
